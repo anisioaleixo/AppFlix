af7aef1814edc72ab5d0de862a38fe067b9e47eb
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-01-19 13:24:29 -0300

    Inicial commmit!

---
+++ app/src/main/java/com/anisioaleixo/appflix/AppFlix.kt
@@ -0,0 +1,12 @@
+package com.anisioaleixo.appflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppFlix : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_home)
+    }
+}
