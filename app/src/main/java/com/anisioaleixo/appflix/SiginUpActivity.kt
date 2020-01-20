9b28ae138aae8855dca72c5dd5fcb29c581a7b53
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-01-19 23:01:46 -0300

    Criando ações dos botões!

--- app/src/main/java/com/anisioaleixo/appflix/SiginUpActivity.kt
+++ app/src/main/java/com/anisioaleixo/appflix/SiginUpActivity.kt
@@ -1,8 +1,10 @@
 package com.anisioaleixo.appflix
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_sigin_up.*
 
 class SiginUpActivity : AppCompatActivity() {
@@ -11,12 +13,36 @@ class SiginUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sigin_up)
 
+        siginup_edt_email.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        siginup_edt_password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        siginup_btn_continue.setOnClickListener() { println("Botão Clicado") }
+
         setSupportActionBar(siginup_toolbar)
         supportActionBar?.title = null
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.siginup_menu,menu)
+        menuInflater.inflate(R.menu.siginup_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
--- app/src/main/java/com/anisioaleixo/appflix/SignUpWelcomeActivity.kt
+++ app/src/main/java/com/anisioaleixo/appflix/SignUpWelcomeActivity.kt
@@ -1,5 +1,6 @@
 package com.anisioaleixo.appflix
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,6 +11,11 @@ class SignUpWelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_welcome)
+
+        siginup_welcome_btn_continue.setOnClickListener(){
+            val intent = Intent(this, SiginUpActivity::class.java)
+            startActivity(intent)
+        }
 
         //carregando barra de navegação com suporte
         setSupportActionBar(siginup_welcome_toolbar)
